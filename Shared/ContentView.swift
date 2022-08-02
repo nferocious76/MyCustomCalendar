a799df91dcfee423e17260382439795aa21e1235
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2022-08-02 13:49:25 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Neil Francis Hipona on 8/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MyCustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomCalendarApp.swift
+//  Shared
+//
+//  Created by Neil Francis Hipona on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
